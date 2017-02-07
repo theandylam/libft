7671ff7202a36b5fdb36cc7040744617613cfbf1
Andy Lam <theandylam@gmail.com>
2017-02-07 15:41:21 -0800

    Changed: ft_putstr now returns amount of chars written

--- ft_putstr.c
+++ ft_putstr.c
@@ -12,16 +12,17 @@
 
 #include "libft.h"
 
-void	ft_putstr(char const *s)
+int	ft_putstr(char const *s)
 {
 	int i;
 
 	if (s == NULL)
-		return ;
+		return (0);
 	i = 0;
 	while (s[i] != '\0')
 	{
 		ft_putchar(s[i]);
 		i++;
 	}
+	return (i);
 }
